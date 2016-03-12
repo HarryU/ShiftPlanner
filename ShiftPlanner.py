8a2f3074d7529b9ed6ededdcb46a033b40626f91
HarryU <hmucs@yahoo.co.uk>
2016-03-12 15:23:04 +0000

    Fixed the eligibility somewhat by checking no shifts are already on that day. Need to expand the criteria to spread out shifts across the month.

--- ShiftPlanner.py
+++ ShiftPlanner.py
@@ -11,9 +11,6 @@ for y in xrange(1, 4):
     y *= boxSize
     blankRotaImage[y, :] = 255
 
-rotaImagePositionDict = {'1': (0, 0), '2': (0, 1), '3': (0, 2), '4': (0, 3), '5': (0, 4), '6': (0, 5), '7': (0, 6),
-                         '8': (1, 0), '9': (1, 1), }
-
 class shiftPlanner:
     def __init__(self, numberOfB6, numberOfB5, numberOfHCA, months):
         self.numberOfB6 = numberOfB6
@@ -42,23 +39,29 @@ class shiftPlanner:
 
     def PopulateRota(self):
         iterations = 0
-        while iterations < 1000:
-            for shift in self.rota.days:
-                while shift.GetNumberOfRank('B6') < 1 and iterations < 1000:
-                    iterations += 1
-                    for nurse in self.staff:
-                        if nurse.IsEligibleForShift(shift, 'B6'):
-                            self.AddNurseToShift(nurse, shift)
-                while shift.GetNumberOfRank('B5') < 4 and iterations < 1000:
-                    iterations += 1
-                    for nurse in self.staff:
-                        if nurse.IsEligibleForShift(shift, 'B5'):
-                            self.AddNurseToShift(nurse, shift)
-                while shift.GetNumberOfRank('HCA') < 1 and iterations < 1000:
-                    iterations += 1
-                    for nurse in self.staff:
-                        if nurse.IsEligibleForShift(shift, 'HCA'):
-                            self.AddNurseToShift(nurse, shift)
+        for shift in self.rota.days:
+            B5s = shift.GetNumberOfRank('B5')
+            B6s = shift.GetNumberOfRank('B6')
+            HCAs = shift.GetNumberOfRank('HCA')
+            while B6s < 1:
+                for nurse in self.staff:
+                    if nurse.IsEligibleForShift(shift, 'B6'):
+                        self.AddNurseToShift(nurse, shift)
+                        B6s += 1
+                        break
+            while B5s < 4:
+                for nurse in self.staff:
+                    if nurse.IsEligibleForShift(shift, 'B5'):
+                        self.AddNurseToShift(nurse, shift)
+                        B5s += 1
+                        break
+            while HCAs < 1:
+                for nurse in self.staff:
+                    if nurse.IsEligibleForShift(shift, 'HCA'):
+                        self.AddNurseToShift(nurse, shift)
+                        HCAs += 1
+                        break
+            print shift.day, B6s, B5s, HCAs
 
 
 class Rota(object):
@@ -104,15 +107,15 @@ class Nurse(object):
         for shift in self.shifts:
             if shift.shiftType == 'LD':
                 hours += 13
-            elif shift.shiftType == 'N':
+            if shift.shiftType == 'N':
                 hours += 12
         return hours
 
     def IsEligibleForShift(self, shift, rank):
         if self.GetHours() < 150:
             days = []
-            for shift in self.shifts:
-                days.append(shift.day)
+            for workedShift in self.shifts:
+                days.append(workedShift.day)
             if shift.day not in days:
                 if self.rank == rank:
                     return True
@@ -120,14 +123,21 @@ class Nurse(object):
             return False
 
     def DisplayRota(self):
-        rotaImage = blankRotaImage
+        rotaImage = np.zeros((boxSize * 4, boxSize * 7, 3))
+        for x in xrange(1, 7):
+            x *= boxSize
+            blankRotaImage[:, x] = 255
+        for y in xrange(1, 4):
+            y *= boxSize
+            rotaImage[y, :] = 255
         for shift in self.shifts:
             row, col = 0, 0
             for i in xrange(1, shift.day):
                 if i % 7.0 == 0:
                     col = 0
                     row += 1
-                col += 1
+                else:
+                    col += 1
             if shift.shiftType == 'LD':
                 rotaImage[(row * boxSize) + 1:(row * boxSize) + boxSize, (col * boxSize) + 1:(col * boxSize) + boxSize] += (0, 0, 255)
             if shift.shiftType == 'N':
@@ -138,19 +148,15 @@ class Nurse(object):
 
 class test_shiftPlanner(unittest.TestCase):
     def setUp(self):
-        self.B6 = 5
-        self.B5 = 19
-        self.HCA = 5
+        self.B6 = 6
+        self.B5 = 25
+        self.HCA = 6
         self.months = 1
         self.ShiftPlanner = shiftPlanner(self.B6, self.B5, self.HCA, self.months)
 
-    # def test_addNurseToShift(self):
-    #     nurse = Nurse('1', 'B6', [])
-    #     self.ShiftPlanner.AddNurseToShift(nurse, self.ShiftPlanner.rota.days[0])
-    #     self.assertEqual(self.ShiftPlanner.rota.days[0].nurses[0], nurse)
-
     def test_showShifts(self):
         self.ShiftPlanner.PopulateRota()
-        for staff in self.ShiftPlanner.rota.days:
-            for nurse in staff.nurses:
-                print nurse.name, ' has ', len(nurse.shifts), ' shift/s this month.'
+        for nurse in self.ShiftPlanner.staff:
+            print nurse.GetHours()
+            nurse.DisplayRota()
+            cv2.destroyAllWindows()
